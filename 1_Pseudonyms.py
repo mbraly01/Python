6b6f0e96c4392b1a688c0e960300675ae5a183b6
mbraly01 <mattbraly2015@gmail.com>
2020-08-15 17:43:09 -0400

    finished Pseudonyms

--- 1_Pseudonyms.py
+++ 1_Pseudonyms.py
@@ -1,4 +1,25 @@
 #Makes pseudonyms out of existing names
 import csv
+import random
 
-with open('./1_Pseudonyms_files/Firstnamse.csv') as f_obj:
+first_path = '/mnt/c/Users/mattb/Desktop/Code/Python/1_Pseudonyms_files/Firstnames.csv' 
+last_path = '/mnt/c/Users/mattb/Desktop/Code/Python/1_Pseudonyms_files/Names_2010Census.csv'
+first_name = []
+last_name = []
+with open(first_path) as f_obj:
+    for line in enumerate(f_obj):
+        seperated_name = (line[1].split(','))[1]
+        seperated_name = seperated_name.split('"')
+        seperated_name = seperated_name[1]
+        first_name.append(seperated_name)
+
+with open(last_path) as f_obj:
+    for line in enumerate(f_obj):
+        seperated_name = (line[1].split(','))[0]
+        seperated_name = seperated_name.lower()
+        seperated_name = seperated_name.capitalize()
+        last_name.append(seperated_name)
+
+print(random.choice(first_name), random.choice(last_name))
+
+        
